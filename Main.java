50711edef5a025e197af5de0953ae52ee6d7c5de
SAKSHI GUPTA <91936750+sakshigupta0809@users.noreply.github.com>
2024-03-19 01:24:14 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,49 +1,51 @@
 import java.util.ArrayList;
-import  java.util.*;
+import java.util.Scanner;
 
-// -----------------------------Employee class ------------------------------
+//-----------employee class which store the detial of full time and part time employee detial--------
 abstract class Employee{
-//    ----------------use access specifier ------------
+//    ----use access specifier ---
     private String name;
     private int id;
-//    ----------------- use constructer ------------------
+//    ----- use constructer --
     public Employee(String name,int id){
         this.name = name;
         this.id = id;
     }
-//    ------------------------- use incapculation ----------------
+// ----- use incapculation ---
     public String getName(){
         return name;
     }
     public int getId(){
         return id;
     }
-//    ------------------------ use abstract method ----------------
+//    ---- use abstract method ----
     public abstract double calculateSalary();
-//    -------------------------- use polymorfism -------------------------
-    @Override
-    public String toString(){
-        return "Employee [name = " +name+ ", id = " +id+ ", salary = " +calculateSalary() +"]";
-    }
+
 }
 
 
-
-//------------------------ inheritence used (full Time Employee class) --------------------
+//------------- inheritence used(full time employee class )--------
 class FullTimeEmployee extends Employee{
     private double monthlySalary;
     public FullTimeEmployee(String name,int id,double monthlySalary){
-//        ----------------------- using super class constracter values by super keyword ------------------
+//        -- using super class constracter values by super keyword ----
         super(name,id);
         this.monthlySalary = monthlySalary;
     }
-//    ------------------------ using override method from super class-----------
+//    ---- using override method from super class----
     @Override
     public double calculateSalary(){
         return monthlySalary;
     }
+
+    @Override
+    public String toString(){
+        return "FullTime Employee [name = " +getName()+ ", id = " +getId()+ ", salary = " +calculateSalary() +"]";
+    }
 }
------------------------- parttimeemployee class inherited from thir supper class Employee-----------------
+
+
+//-------- part time employee extended from employee class----------
 class PartTimeSalary extends Employee{
     private int hoursWorked;
     private double hoursRate;
@@ -57,10 +59,18 @@ class PartTimeSalary extends Employee{
         return hoursWorked*hoursRate;
     }
 
+    @Override
+    public String toString(){
+        return "PartTime Employee [name = " +getName()+ ", id = " +getId()+ ", salary = " +calculateSalary() +"]";
+    }
+
 }
 
-// --------------------------------Payroll System class---------------------------
-// -------------- perform opration like 1.add employee 2.display employee 3.remove employee 4.claculate salary --------------
+//-------PayrollSyatem class which perform some opration like---------
+//1. add employee
+//2. remove employee
+//3. display employee
+//4. calculate salary
 class PayrollSystem{
     private ArrayList<Employee> employeesList;
     public PayrollSystem(){
@@ -87,23 +97,34 @@ class PayrollSystem{
         }
     }
 }
-
-// --------------------------main class where we call other classes objects--------------------
+//---------------------main class which call all classes with their objects----------------------
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         PayrollSystem payroll = new PayrollSystem();
 
-//        System.out.println("which type of employee you want to add: ");
-//        System.out.println("press key for fulltime:1 & parttime:2 ");
-//        int i = sc.nextInt();
-
+        System.out.println("enter full time employee detials: name, id and salary");
         String name = sc.nextLine();
         int id = sc.nextInt();
         double salary = sc.nextDouble();
+        FullTimeEmployee emp1 = new FullTimeEmployee(name, id, salary);
 
-        FullTimeEmployee emp1 = new FullTimeEmployee(name,id,salary);
-        PartTimeSalary emp2 = new PartTimeSalary("shreya",2,3,100);
+        System.out.println("enter part time employee detials: name, id, hourWorked and hourRate ");
+        String name1 = "" ;
+        int hoursWorked = 0;
+        int id2 = 0;
+        double hoursRate = 0.0;
+
+        try {
+             name1 = sc.next();
+             sc.nextLine();
+            id2 = sc.nextInt();
+             hoursWorked = sc.nextInt();
+             hoursRate = sc.nextInt();
+        } catch (Exception e) {
+        }
+
+        PartTimeSalary emp2 = new PartTimeSalary(name1, id2, hoursWorked, hoursRate);
 
         payroll.addEmployee(emp1);
         payroll.addEmployee(emp2);
@@ -111,10 +132,11 @@ public class Main {
         System.out.println("Employee detial: ");
         payroll.displayEmployee();
 
-        payroll.removeEmployee(1);
+        System.out.println("which id you wat to remove");
+        int removeId = sc.nextInt();
+        payroll.removeEmployee(removeId);
         System.out.println("Remaing Employee detial: ");
         payroll.displayEmployee();
-
         
     }
 }
